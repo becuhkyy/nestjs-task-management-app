2fe7eaa18d023e518248d48113d9ff9c466e31dc
Vesko Vasilev <payvesipal@gmail.com>
2024-08-20 17:08:16 +0300

    validate status update input

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,9 +9,10 @@ import {
   Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
+import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTastStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -42,7 +43,11 @@ export class TasksController {
   }
 
   @Patch(':id/status')
-  updateTaskStatus(@Param('id') id, @Body('status') status: TaskStatus): Task {
+  updateTaskStatus(
+    @Param('id') id,
+    @Body() updateTaskStatusDto: UpdateTastStatusDto,
+  ): Task {
+    const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
 }
