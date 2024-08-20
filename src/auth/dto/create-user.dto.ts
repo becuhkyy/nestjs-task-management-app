f8ef6f17d47dfa4107ec425e6662f7497d1dcb0f
Vesko Vasilev <payvesipal@gmail.com>
2024-08-20 22:41:54 +0300

    add input validation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,9 +1,25 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password is too weak. It must contain at least one uppercase letter, one lowercase letter, one number or special character, and be at least 8 characters long.',
+  })
   password: string;
 }
