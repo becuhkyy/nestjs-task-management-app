5592b82ad96b986f8fef32a34bcb150cd02e8bce
Vesko Vasilev <payvesipal@gmail.com>
2024-08-21 15:14:31 +0300

    use an interceptor to serialize the response and exclude user from the tasks

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -12,6 +12,6 @@ export class User {
   @Column()
   password: string;
 
-  @OneToMany(() => Task, (task) => task.user, { eager: false })
+  @OneToMany(() => Task, (task) => task.user, { eager: true })
   tasks: Task[];
 }
