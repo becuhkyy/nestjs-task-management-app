515b15a773c457ea24e49b86ef40758f43445833
Vesko Vasilev <payvesipal@gmail.com>
2024-08-22 20:38:19 +0300

    import config and update typeorm to import async with ConfigModule as a dependency, then inject CM and user the env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks/task.entity';
 import { AuthModule } from './auth/auth.module';
 import { User } from './auth/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     TasksModule,
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'task-management-user',
-      password: 'qi4CbEd6T09bJBfg',
-      database: 'task-management',
-      autoLoadEntities: true,
-      entities: [Task, User],
-      // Should not be set to true in production - it will drop all tables and recreate them
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: true,
+        entities: [Task, User],
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        // Should not be set to true in production - it will drop all tables and recreate them
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [],
